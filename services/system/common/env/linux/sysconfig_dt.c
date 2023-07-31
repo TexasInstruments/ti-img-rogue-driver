d0f9177d0910360dfc08528803291e2c87cd813d
Randolph Sapp <rs@ti.com>
2023-07-31 14:39:14 -0500

    all: add all TI platforms
    Add all the TI specific platforms using links back to the IMG supported
    configs where applicable. Tweak the IMG configs to enable additional
    debug information by default.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- services/system/common/env/linux/sysconfig_dt.c
+++ services/system/common/env/linux/sysconfig_dt.c
@@ -368,7 +368,8 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 			pvOSDevice);
 		if (id) {
 			if (!(strcmp(id->compatible, "ti,j721s2-gpu") ||
-			      strcmp(id->compatible, "ti,j721s2-pvr"))) {
+			      strcmp(id->compatible, "ti,j721s2-pvr") ||
+			      strcmp(id->compatible, "ti,j721e-pvr"))) {
 				(*ppsDevConfig)->eCacheSnoopingMode =
 					PVRSRV_DEVICE_SNOOP_CPU_ONLY;
 			}
