9bb04e59d93adea75b9ecb63816f31feb439b4dd
Randolph Sapp <rs@ti.com>
2025-06-12 18:27:44 -0500

    sysconfig_dt: pick up all ti bxs compatibles
    Add ti,am62p-pvr and ti,j721s2-pvr so that all TI BXS-4-64 instances can
    link to this common path. We'll switch to the upstream bindings in the
    next lts since those are stable now.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- services/system/common/env/linux/sysconfig_dt.c
+++ services/system/common/env/linux/sysconfig_dt.c
@@ -367,7 +367,8 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 			((struct device *)pvOSDevice)->driver->of_match_table,
 			pvOSDevice);
 		if (id) {
-			if (!strcmp(id->compatible, "ti,j721s2-gpu")) {
+			if (!(strcmp(id->compatible, "ti,j721s2-gpu") ||
+			      strcmp(id->compatible, "ti,j721s2-pvr"))) {
 				(*ppsDevConfig)->eCacheSnoopingMode =
 					PVRSRV_DEVICE_SNOOP_CPU_ONLY;
 			}
