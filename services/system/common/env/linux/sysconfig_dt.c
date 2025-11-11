TexasInstruments/ti-img-rogue-driver

commit d0f9177d0910360dfc08528803291e2c87cd813d

all: add all TI platforms
Add all the TI specific platforms using links back to the IMG supported
configs where applicable. Tweak the IMG configs to enable additional
debug information by default.

Signed-off-by: Randolph Sapp <rs@ti.com>

services/system/common/env/linux/sysconfig_dt.c
@@ -368,7 +368,8 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 			pvOSDevice);
 		if (id) {
 			if (!(strcmp(id->compatible, "ti,j721s2-gpu") ||
-			      strcmp(id->compatible, "ti,j721s2-pvr"))) {
+			      strcmp(id->compatible, "ti,j721s2-pvr") ||
+			      strcmp(id->compatible, "ti,j721e-pvr"))) {
 				(*ppsDevConfig)->eCacheSnoopingMode =
 					PVRSRV_DEVICE_SNOOP_CPU_ONLY;
 			}

build/linux/am62p_linux/Makefile
@@ -38,7 +38,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ### ###########################################################################
 
-RGX_BVNC := 33.15.11.3
+RGX_BVNC := 36.53.104.796
 PVR_SYSTEM := rgx_ti_linux
 
 include ../config/preconfig.mk
@@ -50,9 +50,7 @@ endif
 
 NO_HARDWARE := 0
 PVR_LDM_PLATFORM_PRE_REGISTERED := 1
-
 SUPPORT_NATIVE_FENCE_SYNC := 1
-SUPPORT_MIPS_64K_PAGE_SIZE := 1
 
 KERNEL_COMPONENTS := srvkm
 

build/linux/am68_linux/Makefile
@@ -52,6 +52,9 @@ NO_HARDWARE := 0
 PVR_LDM_PLATFORM_PRE_REGISTERED := 1
 SUPPORT_NATIVE_FENCE_SYNC := 1
 
+# HACK to circumvent buffer import issues for now
+SUPPORT_WRAP_EXTMEM := 0
+
 KERNEL_COMPONENTS := srvkm
 
 ifeq ($(PVR_REMVIEW),1)
@@ -61,6 +64,9 @@ ifeq ($(PVR_REMVIEW),1)
 else
  PVR_DRM_MODESET_DRIVER_NAME := tidss
 endif
+
+# Always print fatal and error logs, especially in kernel mode.
+PVRSRV_NEED_PVR_DPF := 1
 
 # Should be last
 include ../config/core.mk

build/linux/j721e_linux/Makefile
@@ -38,7 +38,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ### ###########################################################################
 
-RGX_BVNC := 33.15.11.3
+RGX_BVNC := 22.104.208.318
 PVR_SYSTEM := rgx_ti_linux
 
 include ../config/preconfig.mk

build/linux/j721s2_linux
@@ -1,0 +1,1 @@
+am68_linux

build/linux/j722s_linux
@@ -1,0 +1,1 @@
+am62p_linux/

build/linux/j784s4_linux
@@ -1,0 +1,1 @@
+am68_linux/

services/system/rgx_ti_linux/sysinfo.h
@@ -55,6 +55,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define SYS_RGX_DEV_NAME "ti_pvr_gpu"
 #define SYS_RGX_DMA_WIDTH (40)
 #define SYS_RGX_OF_COMPATIBLE    \
+	_COMPAT("ti,j721e-pvr")  \
 	_COMPAT("ti,am62-gpu")   \
 	_COMPAT("ti,j721s2-gpu") \
 	_COMPAT("ti,j721s2-pvr") \

commit a838ac0074db640ebd1b64be6364417b1bbca3cd

sysconfig_dt: resolve cache snooping logic
The previous two commits were working off of bad logic, considering the
values strcmp should return. We need to set the snoop mode to
PVRSRV_DEVICE_SNOOP_CPU_ONLY for all of the device compatibity strings
listed in the conditional.

Fixes: d0f9177d ("all: add all TI platforms")
Fixes: 9bb04e59 ("sysconfig_dt: pick up all ti bxs compatibles")
Signed-off-by: Randolph Sapp <rs@ti.com>

services/system/common/env/linux/sysconfig_dt.c
@@ -385,8 +385,8 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 			((struct device *)pvOSDevice)->driver->of_match_table,
 			pvOSDevice);
 		if (id) {
-			if (!(strcmp(id->compatible, "ti,j721s2-gpu") ||
-			      strcmp(id->compatible, "ti,j721s2-pvr") ||
+			if (!(strcmp(id->compatible, "ti,j721s2-gpu") &&
+			      strcmp(id->compatible, "ti,j721s2-pvr") &&
 			      strcmp(id->compatible, "ti,j721e-pvr"))) {
 				(*ppsDevConfig)->eCacheSnoopingMode =
 					PVRSRV_DEVICE_SNOOP_CPU_ONLY;