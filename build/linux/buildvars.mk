TexasInstruments/ti-img-rogue-driver

commit 4474c4928f22ad10a5cc4949dfd81d9249fcc5a5

buildvars: macro prefix map to remove refs to build dir
Use macro maps to prevent host build paths from slipping into bins. This
should suppress most build system sanity warnings and help with
reproducible builds.

Signed-off-by: Randolph Sapp <rs@ti.com>

build/linux/buildvars.mk
@@ -140,12 +140,16 @@ TESTED_TARGET_USER_FLAGS := \
  $(call cc-option,-Wno-error=assume) \
  $(call cc-option,-fdiagnostics-show-option) \
  $(call cc-option,-Wno-self-assign) \
- $(call cc-option,-Wno-parentheses-equality)
+ $(call cc-option,-Wno-parentheses-equality) \
+ $(call cc-option,-fmacro-prefix-map=$(srctree)/=) \
+ $(call cc-option,-fdebug-prefix-map=$(srctree)/=)
 TESTED_HOST_USER_FLAGS := \
  $(call host-cc-option,-Wno-missing-field-initializers) \
  $(call host-cc-option,-fdiagnostics-show-option) \
  $(call host-cc-option,-Wno-self-assign) \
- $(call host-cc-option,-Wno-parentheses-equality)
+ $(call host-cc-option,-Wno-parentheses-equality) \
+ $(call host-cc-option,-fmacro-prefix-map=$(srctree)/=) \
+ $(call host-cc-option,-fdebug-prefix-map=$(srctree)/=)
 
 # These flags are clang-specific.
 # -Wno-unused-command-line-argument works around a buggy interaction
@@ -262,6 +266,12 @@ TESTED_KBUILD_FLAGS := \
 TESTED_KBUILD_FLAGS := \
  $(call kernel-cc-option,-fno-pie) \
  $(call kernel-cc-option,-no-pie) \
+ $(TESTED_KBUILD_FLAGS)
+
+# Use macro prefix map to prevent host paths from leaking into bins
+TESTED_KBUILD_FLAGS := \
+ $(call kernel-cc-option,-fmacro-prefix-map=$(srctree)/=) \
+ $(call kernel-cc-option,-fdebug-prefix-map=$(srctree)/=) \
  $(TESTED_KBUILD_FLAGS)
 
 # When building against experimentally patched kernels with LLVM support,

commit 1973210486a08665d716fc79816df3c6246e86c8

buildvars: fix prefix-maps to prevent references
Adjust the macro-prefix-map and debug-prefix-map values to use the main
project TOP directory. This path is already full and shouldn't require
any modification.

Also, pass ALL_KBUILD_CFLAGS to kbuild with EXTRA_CFLAGS instead of
INTERNAL_KBUILD_CFLAGS. If we don't do this, certain flags like
macro-prefix-map and debug-prefix-map will not be considered when
processing the modfinal step.

Additionally, these are validated global flags. No reason to append them
to a single module build target. Remove the now redundant
INTERNAL_KBUILD_CFLAGS logic as a result.

Fixes: 4474c492 ("buildvars: macro prefix map to remove refs to build dir")
Signed-off-by: Randolph Sapp <rs@ti.com>

build/linux/buildvars.mk
@@ -141,15 +141,15 @@ TESTED_TARGET_USER_FLAGS := \
  $(call cc-option,-fdiagnostics-show-option) \
  $(call cc-option,-Wno-self-assign) \
  $(call cc-option,-Wno-parentheses-equality) \
- $(call cc-option,-fmacro-prefix-map=$(srctree)/=) \
- $(call cc-option,-fdebug-prefix-map=$(srctree)/=)
+ $(call cc-option,-fmacro-prefix-map=$(TOP)/=) \
+ $(call cc-option,-fdebug-prefix-map=$(TOP)/=)
 TESTED_HOST_USER_FLAGS := \
  $(call host-cc-option,-Wno-missing-field-initializers) \
  $(call host-cc-option,-fdiagnostics-show-option) \
  $(call host-cc-option,-Wno-self-assign) \
  $(call host-cc-option,-Wno-parentheses-equality) \
- $(call host-cc-option,-fmacro-prefix-map=$(srctree)/=) \
- $(call host-cc-option,-fdebug-prefix-map=$(srctree)/=)
+ $(call host-cc-option,-fmacro-prefix-map=$(TOP)/=) \
+ $(call host-cc-option,-fdebug-prefix-map=$(TOP)/=)
 
 # These flags are clang-specific.
 # -Wno-unused-command-line-argument works around a buggy interaction
@@ -270,8 +270,8 @@ TESTED_KBUILD_FLAGS := \
 
 # Use macro prefix map to prevent host paths from leaking into bins
 TESTED_KBUILD_FLAGS := \
- $(call kernel-cc-option,-fmacro-prefix-map=$(srctree)/=) \
- $(call kernel-cc-option,-fdebug-prefix-map=$(srctree)/=) \
+ $(call kernel-cc-option,-fmacro-prefix-map=$(TOP)/=) \
+ $(call kernel-cc-option,-fdebug-prefix-map=$(TOP)/=) \
  $(TESTED_KBUILD_FLAGS)
 
 # When building against experimentally patched kernels with LLVM support,

build/linux/kbuild/Makefile.template
@@ -91,8 +91,6 @@ endif
 ccflags-y += -D__linux__ -include $(OUT)/config_kernel.h \
  -I$(OUT)/include
 
-ccflags-y += $(INTERNAL_KBUILD_CFLAGS)
-
 ifeq ($(DEFINE_X86_FEATURE_LA57),1)
  ccflags-$(CONFIG_X86) += -include $(srctree)/arch/x86/include/asm/cpufeatures.h
 endif

build/linux/kbuild/kbuild.mk
@@ -86,7 +86,6 @@ kbuild_check:
 kbuild: kbuild_check $(TARGET_PRIMARY_OUT)/kbuild/Makefile bridges
 	$(if $(V),,@)$(MAKE) -Rr --no-print-directory -C $(KERNELDIR) \
 		M=$(abspath $(TARGET_PRIMARY_OUT)/kbuild) \
-		INTERNAL_KBUILD_CFLAGS="$(ALL_KBUILD_CFLAGS)" \
 		INTERNAL_KBUILD_MAKEFILES="$(INTERNAL_KBUILD_MAKEFILES)" \
 		INTERNAL_KBUILD_OBJECTS="$(INTERNAL_KBUILD_OBJECTS)" \
 		INTERNAL_EXTRA_KBUILD_OBJECTS="$(INTERNAL_EXTRA_KBUILD_OBJECTS)" \
@@ -98,6 +97,7 @@ kbuild: kbuild_check $(TARGET_PRIMARY_OUT)/kbuild/Makefile bridges
 		%-androideabi,%-gnueabi,$(CROSS_TRIPLE)),$(patsubst \
 		%-android,%-gnu,$(CROSS_TRIPLE)))- \
 		CROSS_COMPILE="$(KERNEL_CROSS_COMPILE)" \
+		EXTRA_CFLAGS="$(ALL_KBUILD_CFLAGS)" \
 		CC=$(if $(KERNEL_CC),$(KERNEL_CC),$(KERNEL_CROSS_COMPILE)gcc) \
 		AR=$(if $(KERNEL_AR),$(KERNEL_AR),$(KERNEL_CROSS_COMPILE)ar) \
 		LD=$(if $(KERNEL_LD),$(KERNEL_LD),$(KERNEL_CROSS_COMPILE)ld) \