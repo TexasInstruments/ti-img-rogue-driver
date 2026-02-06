d241b0d5df404c60b80233bb5116b596039062e6
Antonios Christidis <a-christidis@ti.com>
2026-02-06 15:22:05 -0600

    system: Unify device tree compatible strings
    Unify device tree compatible strings for J721e,am62p.
    
    Signed-off-by: Antonios Christidis <a-christidis@ti.com>

--- services/system/common/env/linux/sysconfig_dt.c
+++ services/system/common/env/linux/sysconfig_dt.c
@@ -418,6 +418,7 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 		{
 			if (!(strcmp(id->compatible, "ti,j721s2-gpu") &&
 			      strcmp(id->compatible, "ti,j721s2-pvr") &&
+			      strcmp(id->compatible, "ti,j721e-gpu") &&
 			      strcmp(id->compatible, "ti,j721e-pvr"))) 
 			{
 				(*ppsDevConfig)->eCacheSnoopingMode = PVRSRV_DEVICE_SNOOP_CPU_ONLY;
